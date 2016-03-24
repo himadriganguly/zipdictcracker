dd5019045b662bc2450e7ea021130dc016410f52
root <root@localhost.localdomain>
2016-03-25 04:14:59 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Dictionary Based ZipFile Password Cracker In Python
+
+This application is developed just for fun and learning Python.
+
+
+## Requirements
+
+1. Python3
+
+
+## Usage
+
+python3 zipdictcracker.py -h
+
+To get the help menu
+
+python3 zipdictcracker.py secure.zip dictionary.txt
+
+It will check for all the words in the dictionary to crack the zip file and if found it will extract the files from the zip.
+
+The password for the zip file that is included is `secret`
+
+
+## Contribute
+
+If you have any idea you want to implement or think that there is a better way to implement any part of the code please create a fork and I will do my best to merge appropriately. Thank you.
+
+Hope you will like the application.
---
+++ zipdictcracker.py
@@ -0,0 +1,67 @@
+import zipfile
+import sys
+import threading
+from threading import Thread
+import argparse
+
+found = False
+
+def extractFile(zFile, password, lock, passFile):
+	with lock:
+		try:
+			print('Checking With Password: {}'.format(password.decode('utf-8')))
+			zFile.extractall(pwd=password)
+			print('===========================================')
+			print('\n[+] Password Found = {}\n'.format(password.decode('utf-8')))
+			print('===========================================')
+			global found
+			found = True
+		except:
+			pass
+			
+def main():
+	parser = argparse.ArgumentParser('Dictionary Based Zip File Password Cracker')
+	parser.add_argument('file', type=str, help='The Zip File Name. You can also provide the full path of the ZipFile')
+	parser.add_argument('dictionary', type=str, help='The Dictionary File Name. You can also provide the full path of the Dictionary File')
+	args = parser.parse_args()
+	
+	global found
+
+	try:
+		print('===========================================')
+		print('\nLoding Files\n')
+		print('===========================================')
+		zFile = zipfile.ZipFile(args.file)
+		passFile = open(args.dictionary, 'rb')
+	except Exception as e:
+		print(e)
+		sys.exit(1)
+	
+	print('\nFiles Loaded\n')
+	print('===========================================')
+	print('\nCracking Password Using Dictionary Attack\n')
+	print('===========================================\n')
+	lock = threading.Lock()
+	for line in passFile:
+		password = line.strip()
+		# print('{}\r'.format(password))
+		if found != True:
+			t = Thread(target=extractFile, args=(zFile, password, lock, passFile))
+			t.start()
+		else:
+			passFile.close()
+			print('\n===========================================\n')
+			print('Exiting From Application\n')
+			print('===========================================\n')
+			sys.exit()
+	
+	while (threading.active_count() > 1):
+		if threading.active_count() == 1 and found != True:
+			print(found)
+			print('===========================================')
+			print('\nPassword Not Found In Dictionary\n')
+			print('===========================================')
+			sys.exit()
+			
+if __name__ == '__main__':
+	main()
